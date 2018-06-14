705eb09afa7b65ba4854411d94212a15d9203f39
iknox27 <acam27@hotmail.com>
2018-06-14 10:59:11 -0600

    txt

--- app/src/main/java/com/fireblend/uitest/ui/DetallesContactoActivity.java
+++ app/src/main/java/com/fireblend/uitest/ui/DetallesContactoActivity.java
@@ -1,7 +1,13 @@
 package com.fireblend.uitest.ui;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Environment;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -18,6 +25,10 @@ import com.fireblend.uitest.entities.Contact;
 import com.fireblend.uitest.helpers.DataBaseManager;
 import com.fireblend.uitest.helpers.PreferencesManager;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -30,6 +41,7 @@ public class DetallesContactoActivity extends AppCompatActivity {
     MaterialDialog materialDialog;
     PreferencesManager preferencesManager;
     private static final long SPLASH_SCREEN_DELAY = 3000;
+    private static final int PERM_CODE = 1001;
     DataBaseManager dataBaseManager;
     @BindView(R.id.nameDetail)
     TextView nameDetail;
@@ -76,9 +88,48 @@ public class DetallesContactoActivity extends AppCompatActivity {
     }
 
 
-    @OnClick(R.id.buttonDel)
+    @OnClick(R.id.descargar)
     public void descargar(){
+        int permissionCheck = ContextCompat.checkSelfPermission(DetallesContactoActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED){
+            StringBuilder sb = new StringBuilder();
+            sb.append("Nombre: " + nameDetail.getText().toString());
+            sb.append("Num Tel: " + numDeta.getText().toString());
+            sb.append("Correo: " + emailD.getText().toString());
+            sb.append("Edad: " + ageDET.getText().toString());
+            sb.append("Provincia: " + provD.getText().toString());
+            generateNoteOnSD(getApplicationContext(),nameDetail.getText().toString() + numDeta.getText().toString() + ".txt", sb.toString() );
+        } else {
+            //Si no, pedimos permiso
+            askForPermission();
+        }
+    }
 
+    private void askForPermission() {
+        //Se solicita permiso. Esta llamada es asincronica, por lo que tenemos que
+        //implementar el metodo callback onRequestPermissionResult para procesar la
+        //respuesta del usuario (ver abajo)
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                PERM_CODE);
+    }
+
+    public void generateNoteOnSD(Context context, String sFileName, String sBody) {
+        try {
+            File root = new File(Environment.getExternalStorageDirectory(), "Downloads");
+            if (!root.exists()) {
+                root.mkdirs();
+            }
+            File gpxfile = new File(root, sFileName);
+            FileWriter writer = new FileWriter(gpxfile);
+            writer.append(sBody);
+            writer.flush();
+            writer.close();
+            Toast.makeText(context, "Se ha guardado", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @OnClick(R.id.buttonDel)
@@ -132,6 +183,18 @@ public class DetallesContactoActivity extends AppCompatActivity {
         }
     }
 
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //Si recibimos al menos un permiso y su valor es igual a PERMISSION_GRANTED, tenemos permiso
+        if (grantResults.length > 0 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                descargar.callOnClick();
+        } else {
+            Toast.makeText(this, ":(", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 
 }
